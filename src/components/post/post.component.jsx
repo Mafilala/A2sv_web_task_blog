d63f52f1356fdd5b0c7ecb32d21b256a0fbe3700
Maruf <marufsherefa9@gmail.com>
2023-09-08 20:36:04 +0300

    added like functionality

--- src/components/post/post.component.jsx
+++ src/components/post/post.component.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import "./post.styles.css";
 import Author from "../author/author.component";
 import Title from "../title/title.component";
 import Summary from "../summary/summary.compoent";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faComment } from "@fortawesome/free-regular-svg-icons";
+import { faThumbsUp } from "@fortawesome/free-regular-svg-icons";
 import {} from "@fortawesome/free-solid-svg-icons";
 
 const Post = ({ author, title, summary, postImgUrl }) => {
+  const [likes, setLikes] = useState(0);
   return (
     <div className="post_and_footer">
       <div className="post">
@@ -25,9 +27,17 @@ const Post = ({ author, title, summary, postImgUrl }) => {
       </div>
       <br />
       <footer>
-        <div className="comment">
-          <FontAwesomeIcon icon={faComment} />
-          <p>2M comments</p>
+        <div
+          className={likes === 0 ? "likes" : "likes active"}
+          onClick={() =>
+            setLikes((prev) => {
+              const l = prev === 0 ? 1 : 0;
+              return l;
+            })
+          }
+        >
+          <FontAwesomeIcon icon={faThumbsUp} />
+          <p>{likes} likes </p>
         </div>
         <button>Read More</button>
       </footer>
