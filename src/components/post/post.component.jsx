5818123ffde2088e2158720dee1d45c12ce60841
Maruf <marufsherefa9@gmail.com>
2023-09-08 16:18:51 +0300

    added comment section and see more button on post component

--- src/components/post/post.component.jsx
+++ src/components/post/post.component.jsx
@@ -2,22 +2,35 @@ import "./post.styles.css";
 import Author from "../author/author.component";
 import Title from "../title/title.component";
 import Summary from "../summary/summary.compoent";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faComment } from "@fortawesome/free-regular-svg-icons";
+import {} from "@fortawesome/free-solid-svg-icons";
 
 const Post = ({ author, title, summary, postImgUrl }) => {
   return (
-    <div className="post">
-      <div className="post-info">
-        <Author
-          imgUrl={author.imgUrl}
-          firstName={author.firstName}
-          lastName={author.lastName}
-        />
-        <Title title={title} />
-        {summary && <Summary summary={summary} />}
+    <div className="post_and_footer">
+      <div className="post">
+        <div className="post-info">
+          <Author
+            imgUrl={author.imgUrl}
+            firstName={author.firstName}
+            lastName={author.lastName}
+          />
+          <Title title={title} />
+          {summary && <Summary summary={summary} />}
+        </div>
+        <div className="post-image-container">
+          <img src={postImgUrl} />
+        </div>
       </div>
-      <div className="post-image-container">
-        <img src={postImgUrl} />
-      </div>
+      <br />
+      <footer>
+        <div className="comment">
+          <FontAwesomeIcon icon={faComment} />
+          <p>2M comments</p>
+        </div>
+        <button>Read More</button>
+      </footer>
     </div>
   );
 };
